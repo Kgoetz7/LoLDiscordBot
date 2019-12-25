96a3ac33e55e74e95a6129635640002fac7aec0e
Kgoetz7 <hockeystarkobe@gmail.com>
2019-12-25 12:47:38 -0700

    Add files via upload

--- DiscordLolRankBot.py
+++ DiscordLolRankBot.py
@@ -10,29 +10,31 @@ async def on_ready():
     print("Bot is ready")
 
 @client.command()
-async def info(pra):
-    output = discord.Embed(title="League Rank Bot", description="Gets LoL account rank from OP.GG", color=0x34a1eb)
-    output.add_field(name="Author", value="TestieGoetze")
-    output.add_field(name="Invite", value="[Invite link]")
-    await pra.send(embed=output)
+async def info(self):
+    ''' When called will return info on the bot '''
+    output = discord.Embed(title='League Rank Bot', description='Gets LoL account rank from OP.GG', color=0x34a1eb)
+    output.add_field(name='Author', value='TestieGoetze')
+    output.add_field(name='Invite', value='https://discordapp.com/api/oauth2/authorize?client_id=654394113499725824&permissions=83968&scope=bot')
+    await self.send(embed=output)
 
 @client.command()
-async def rank(pra, acct):
-    AcctURL = ('https://na.op.gg/summoner/userName=' + acct)
-    page = requests.get(AcctURL)
-
-    AcctLvL = BeautifulSoup(page.content, 'html.parser').find('span', attrs= {"title": "Level"})
-    AcctLvL = ''.join(list(BeautifulSoup.prettify(AcctLvL)[40:-9]))
-    print(AcctLvL)
-    AcctLP = BeautifulSoup(page.content, 'html.parser').find('span', attrs={"class": "LeaguePoints"})
-    AcctLP = ''.join(list(BeautifulSoup.prettify(AcctLP)[28:-9]))
-    AcctName = BeautifulSoup(page.content, 'html.parser').find('span', attrs={"class": "Name"})
-    AcctName = ''.join(list(BeautifulSoup.prettify(AcctName)[21:-9]))
-    AcctRank = BeautifulSoup(page.content, 'html.parser').find('div', attrs={"class": "TierRank"})
-    AcctRank = ''.join(list(BeautifulSoup.prettify(AcctRank)[24:-8]))
-    if int(AcctLvL) < 30:
-        await pra.send(AcctURL + '\n' + 'Summoner Name: ' + AcctName + '\n' + 'Current Rank: UNRANKED')
-    await pra.send(AcctURL + '\n' + 'Summoner Name: ' + AcctName + '\n' + 'Current Rank: ' + AcctRank + '-' + AcctLP)
-
-client.run("NjU0Mzk0MTEzNDk5NzI1ODI0.XfFmYQ.b6ioEx_Um0dyX28SiIManO9U7Yo")
-#<span class="Level tip tpd-delegation-uid-1" title>146</span>
+async def rank(self, *accts):
+    ''' When called in discord will webscrape a players OP.GG rank and account link '''
+    for acct in accts:
+        aurl = ('https://na.op.gg/summoner/userName=' + acct)
+        page = requests.get(aurl)
+        try:
+            an = BeautifulSoup(page.content, 'html.parser').find('span', attrs={'class': 'Name'})
+            an = ''.join(list(BeautifulSoup.prettify(an)[21:-9]))
+        except AttributeError as error:
+            await self.send('The account' + '"' + acct + '"' + 'was not found')
+        try:
+            ar = BeautifulSoup(page.content, 'html.parser').find('div', attrs={'class': 'TierRank'})
+            ar = ''.join(list(BeautifulSoup.prettify(ar)[24:-8]))
+            alp = BeautifulSoup(page.content, 'html.parser').find('span', attrs={"class": "LeaguePoints"})
+            alp = ''.join(list(BeautifulSoup.prettify(alp)[28:-9]))
+            await self.send(aurl + '\n' + 'Summoner Name: ' + an + '\n' + 'Current Rank: ' + ar + '-' + alp)
+        except AttributeError as error:
+                await self.send(aurl + '\n' + 'Summoner Name: ' + an + '\n' + 'Current Rank: ' + (ar[9:]))
+	    
+client.run("NjU0Mzk0MTEzNDk5NzI1ODI0.XffA5A.Vc3O9a2T3nOa9Q9ReuIH21tbpPU")
